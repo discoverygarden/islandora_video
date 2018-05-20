4cb9524b55363afb8ef6b5e66844e05eb2a9cc82
willtp87 <willtp87@hotmail.com>
2018-05-20 14:51:57 -0300

    coder

--- README.md
+++ README.md
@@ -17,14 +17,16 @@ This module requires the following modules/libraries:
 
 ### Installing FFmpeg and AAC Encoders
 
-FFmpeg is a command-line video-processing library, required to create TN, MP4, and MKV derivatives.
-This module has been tested with FFmpeg version 1.1.4. It can be downloaded
-[here](http://www.ffmpeg.org/releases/ffmpeg-1.1.4.tar.gz)
+FFmpeg is a command-line video-processing library, required to create TN, MP4,
+and MKV derivatives. This module has been tested with FFmpeg version 1.1.4. It
+can be downloaded [here](http://www.ffmpeg.org/releases/ffmpeg-1.1.4.tar.gz).
 
-To support the creation of MP4 files, FFmpeg needs an [AAC encoder](https://trac.ffmpeg.org/wiki/Encode/AAC).
-For legacy reasons, this module uses `libfaac` by default. **Libfaac is not free for commercial use**. Alternate encoders
-(such as `libfdk_aac`) may be set in the configuration options. However, due to license restrictions, the mentioned encoders
-are not bundled with FFmpeg and must be enabled when FFmpeg is compiled from source.
+To support the creation of MP4 files, FFmpeg needs an [AAC
+encoder](https://trac.ffmpeg.org/wiki/Encode/AAC). For legacy reasons, this
+module uses `libfaac` by default. **Libfaac is not free for commercial use**.
+Alternate encoders (such as `libfdk_aac`) may be set in the configuration
+options. However, due to license restrictions, the mentioned encoders are not
+bundled with FFmpeg and must be enabled when FFmpeg is compiled from source.
 
 Compilation guides:
 * [Ubuntu](https://trac.ffmpeg.org/wiki/CompilationGuide/Ubuntu)
@@ -72,19 +74,6 @@ If you would like to contribute to this module, please check out the helpful
 [Developers](http://islandora.ca/developers) section on Islandora.ca and create
 an issue, pull request and or contact
 [discoverygarden](http://support.discoverygarden.ca).
-
-### Load Testing
-
-The Video Solution Pack includes an ingest/purge load test that is disabled and unusable by default. This particular solution pack was chosen for load testing due to the resource-heavy process of creating video derivatives and ingesting several potentially-massive files. To enable and use it:
-
-1. Make sure that the [Testing](https://www.drupal.org/docs/7/testing/testing-module) module is enabled and that all prerequisites for derivative creation are satisfied for the Video Solution Pack.
-2. Edit the islandora_video.info file in this folder, removing the ; and whitespace before the line `files[] = tests/islandora_video_load_test.test`
-3. Create a folder in the `tests/fixtures/` called `load`, and place as many videos in that folder as you would like to ingest. These files all must fit within the size specified by your php.ini file's maximum post size, and they must have a file extension supported by the Video Solution Pack, otherwise they will not be picked up by the load test.
-4. Clear the Drupal cache.
-
-The video load test should now be available to use by navigating to `http://path.to.your.site/admin/config/development/testing`, checking 'Video Load Test' in the Islandora Video section, and then clicking 'Run tests'. The test will compare the original file to the file ingested into Fedora to see if they match.
-
-**NOTE**: THIS WILL NOT WORK ON OPERATING SYSTEMS THAT DO NOT SUPPORT THE PHP GLOB_BRACE FLAG (E.G. SOLARIS).
 
 ## License
 
--- src/Form/Admin.php
+++ src/Form/Admin.php
@@ -6,6 +6,9 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element;
 
+/**
+ * Module settings form.
+ */
 class Admin extends ConfigFormBase {
 
   /**
@@ -40,6 +43,9 @@ class Admin extends ConfigFormBase {
     return ['islandora_video.settings'];
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
     module_load_include('inc', 'islandora', 'includes/solution_packs');
     $form = [];
@@ -51,34 +57,34 @@ class Admin extends ConfigFormBase {
     $form['actions'] = ['#type' => 'actions'];
     $form['actions']['reset'] = [
       '#type' => 'submit',
-      '#value' => t('Reset to defaults'),
+      '#value' => $this->t('Reset to defaults'),
       '#weight' => 1,
       '#submit' => [
-        'islandora_video_admin_submit'
-        ],
+        'islandora_video_admin_submit',
+      ],
     ];
 
     // Playback options.
     $form['playback'] = [
       '#type' => 'fieldset',
-      '#title' => t('Playback'),
+      '#title' => $this->t('Playback'),
       '#collapsible' => FALSE,
       '#collapsed' => FALSE,
     ];
-    $description = t("When Off (default), only the MP4 datastream will be played in a viewer (if a viewer is configured above). With this box checked, the uploaded file (OBJ datastream) will be played if:");
-    $description .= '<ul><li>' . t('the MP4 datastream is not present') . '</li>';
-    $description .= '<li>' . t('the OBJ datastream has mimetype video/mp4') . '</li>';
-    $description .= '<li>' . t('the OBJ datastream is smaller than the size configured below:') . '</li></ul>';
+    $description = $this->t("When Off (default), only the MP4 datastream will be played in a viewer (if a viewer is configured above). With this box checked, the uploaded file (OBJ datastream) will be played if:");
+    $description .= '<ul><li>' . $this->t('the MP4 datastream is not present') . '</li>';
+    $description .= '<li>' . $this->t('the OBJ datastream has mimetype video/mp4') . '</li>';
+    $description .= '<li>' . $this->t('the OBJ datastream is smaller than the size configured below:') . '</li></ul>';
     $form['playback']['islandora_video_play_obj'] = [
       '#type' => 'checkbox',
-      '#title' => t('Play OBJ datastream in viewer'),
+      '#title' => $this->t('Play OBJ datastream in viewer'),
       '#description' => $description,
       '#default_value' => \Drupal::config('islandora_video.settings')->get('islandora_video_play_obj'),
     ];
     $form['playback']['islandora_video_max_obj_size'] = [
       '#type' => 'textfield',
-      '#title' => t('Maximum file size for playing OBJ (in MB)'),
-      '#description' => t("Set a maximum size (in megabytes) for sending the OBJ datastream to the player."),
+      '#title' => $this->t('Maximum file size for playing OBJ (in MB)'),
+      '#description' => $this->t("Set a maximum size (in megabytes) for sending the OBJ datastream to the player."),
       '#default_value' => \Drupal::config('islandora_video.settings')->get('islandora_video_max_obj_size'),
       '#states' => [
         'visible' => [
@@ -91,37 +97,37 @@ class Admin extends ConfigFormBase {
     // Derivative Options.
     $form['derivatives'] = [
       '#type' => 'fieldset',
-      '#title' => t('Derivatives'),
+      '#title' => $this->t('Derivatives'),
       '#collapsible' => FALSE,
       '#collapsed' => FALSE,
     ];
     // FFMPEG configuration.
     $form['derivatives']['islandora_video_ffmpeg_path'] = [
       '#type' => 'textfield',
-      '#title' => t('Path to FFmpeg'),
+      '#title' => $this->t('Path to FFmpeg'),
       '#default_value' => \Drupal::config('islandora_video.settings')->get('islandora_video_ffmpeg_path'),
-      '#description' => t('Path to the FFmpeg binary. For example: <kbd>/usr/local/bin/ffmpeg</kbd>  . Used if creating TN, MP4, or MKV derivatives.'),
+      '#description' => $this->t('Path to the FFmpeg binary. For example: <kbd>/usr/local/bin/ffmpeg</kbd>  . Used if creating TN, MP4, or MKV derivatives.'),
       '#required' => TRUE,
     ];
     // TN derivatives.
     $form['derivatives']['islandora_video_make_thumbnail_locally'] = [
       '#type' => 'checkbox',
-      '#title' => t('Create TN datastream locally'),
-      '#description' => t('If On (default), the Drupal server will use FFmpeg to create a thumbnail screenshot from the video, or fall back to a default thumbnail. If Off, no thumbnail will be created.'),
+      '#title' => $this->t('Create TN datastream locally'),
+      '#description' => $this->t('If On (default), the Drupal server will use FFmpeg to create a thumbnail screenshot from the video, or fall back to a default thumbnail. If Off, no thumbnail will be created.'),
       '#default_value' => \Drupal::config('islandora_video.settings')->get('islandora_video_make_thumbnail_locally'),
     ];
     // MP4 derivatives.
     $form['derivatives']['islandora_video_make_mp4_locally'] = [
       '#type' => 'checkbox',
-      '#title' => t('Create MP4 datastream locally'),
-      '#description' => t('If On (default), the Drupal server will use FFmpeg to create a web-friendly mp4 version of the uploaded file. Disable if another server creates derivatives, or if you intend to play the uploaded files directly (see Playback options above). This feature requires a valid MP4 audio codec.'),
+      '#title' => $this->t('Create MP4 datastream locally'),
+      '#description' => $this->t('If On (default), the Drupal server will use FFmpeg to create a web-friendly mp4 version of the uploaded file. Disable if another server creates derivatives, or if you intend to play the uploaded files directly (see Playback options above). This feature requires a valid MP4 audio codec.'),
       '#default_value' => \Drupal::config('islandora_video.settings')->get('islandora_video_make_mp4_locally'),
     ];
     $form['derivatives']['islandora_video_mp4_audio_codec'] = [
       '#type' => 'textfield',
-      '#title' => t('MP4 audio codec'),
-      '#description' => t("Defaults to libfaac, a non-free encoder. FFmpeg must have been compiled from source with that encoder enabled. See @FFMPEG for more info.", [
-        '@FFMPEG' => \Drupal::l(t("FFmpeg's AAC encoding guide"), \Drupal\Core\Url::fromUri('https://trac.ffmpeg.org/wiki/Encode/AAC'))
+      '#title' => $this->t('MP4 audio codec'),
+      '#description' => $this->t("Defaults to libfaac, a non-free encoder. FFmpeg must have been compiled from source with that encoder enabled. See @FFMPEG for more info.", [
+        '@FFMPEG' => \Drupal::l($this->t("FFmpeg's AAC encoding guide"), \Drupal\Core\Url::fromUri('https://trac.ffmpeg.org/wiki/Encode/AAC'))
         ]),
       '#default_value' => \Drupal::config('islandora_video.settings')->get('islandora_video_mp4_audio_codec'),
       '#states' => [
@@ -135,21 +141,21 @@ class Admin extends ConfigFormBase {
     // MKV derivatives.
     $form['derivatives']['islandora_video_make_archive'] = [
       '#type' => 'checkbox',
-      '#title' => t('Create MKV datastream locally'),
-      '#description' => t('If On (default), the Drupal server will use FFmpeg to create a Matroska container derivative. This may be useful for archival purposes, but is not used by Islandora.'),
+      '#title' => $this->t('Create MKV datastream locally'),
+      '#description' => $this->t('If On (default), the Drupal server will use FFmpeg to create a Matroska container derivative. This may be useful for archival purposes, but is not used by Islandora.'),
       '#default_value' => \Drupal::config('islandora_video.settings')->get('islandora_video_make_archive'),
     ];
     // OGG/Theora derivatives.
     $form['derivatives']['islandora_video_make_ogg_locally'] = [
       '#type' => 'checkbox',
-      '#title' => t('Create OGG datastream locally'),
-      '#description' => t("If On (default is Off), the Drupal server will use ffmpeg2theora to create an Ogg Theora container derivative. This is a legacy option only, as this datastream is no longer used by Islandora."),
+      '#title' => $this->t('Create OGG datastream locally'),
+      '#description' => $this->t("If On (default is Off), the Drupal server will use ffmpeg2theora to create an Ogg Theora container derivative. This is a legacy option only, as this datastream is no longer used by Islandora."),
       '#default_value' => \Drupal::config('islandora_video.settings')->get('islandora_video_make_ogg_locally'),
     ];
     $form['derivatives']['islandora_video_ffmpeg2theora_path'] = [
       '#type' => 'textfield',
-      '#title' => t('Path to ffmpeg2theora executable'),
-      '#description' => t('Path to Theora conversion program on your server'),
+      '#title' => $this->t('Path to ffmpeg2theora executable'),
+      '#description' => $this->t('Path to Theora conversion program on your server'),
       '#default_value' => \Drupal::config('islandora_video.settings')->get('islandora_video_ffmpeg2theora_path'),
       '#states' => [
         'visible' => [
